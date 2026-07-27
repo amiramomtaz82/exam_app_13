5e794d430e0888c5b435e15be21ee036b6f715b3
Amira Momtaz <amiraomtaz82@gmail.com>
2026-07-27 15:39:46 +0300

    creating shared files of daa source and repo , app text field, app routes,

---
+++ lib/core/app_routes/app_routes.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../config/di/di.dart';
+import '../../features/auth/presentation/login/view/login_screen.dart';
+import '../../features/auth/presentation/register/view/register_screen.dart';
+
+
+
+abstract final class AppRoutes {
+  static MaterialPageRoute get login =>
+      MaterialPageRoute(builder: (_) => LoginScreen());
+
+  static MaterialPageRoute get register =>
+      MaterialPageRoute(builder: (_) => RegisterScreen());
+
+
+}
---
+++ lib/features/auth/api/datasource/remote_data/auth_remote_data_source_imp.dart
@@ -0,0 +1,8 @@
+import 'package:injectable/injectable.dart';
+
+import '../../../data/datasource/remote_data/auth_remote_data_source.dart';
+
+@LazySingleton(as: AuthRemoteDataSource)
+class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
+
+}
