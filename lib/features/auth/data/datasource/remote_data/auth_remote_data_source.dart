1f39dfbdb72258d5d6bc21cfbd67cc5a8c9b2394
Amira Momtaz <amiraomtaz82@gmail.com>
2026-07-28 09:57:17 +0300

    finished auth data remote imp , auth repo imp for forget password feature

--- lib/features/auth/data/datasource/remote_data/auth_remote_data_source.dart
+++ lib/features/auth/data/datasource/remote_data/auth_remote_data_source.dart
@@ -1,2 +1,24 @@
+import '../../../../../config/base_response/base_response.dart';
+import '../../model/requests/forget_password_request.dart';
+import '../../model/requests/reset_code_request.dart';
+import '../../model/requests/reset_password_request.dart';
+import '../../model/responses/forget_password_response.dart';
+import '../../model/responses/reset_code_response.dart';
+import '../../model/responses/reset_password_response.dart';
+
 abstract class AuthRemoteDataSource {
+
+  Future<BaseResponse<ForgetPasswordResponse>>
+  forgetPassword(ForgetPasswordRequest request);
+
+
+  Future<BaseResponse<ResetPasswordResponse>> restPassword(
+      ResetPasswordRequest request,
+      );
+
+
+  Future<BaseResponse< ResetCodeResponse>> restCode(
+      ResetCodeRequest request,
+      );
+
 }
--- lib/features/auth/domain/repo/auth_repo.dart
+++ lib/features/auth/domain/repo/auth_repo.dart
@@ -1,2 +1,11 @@
+import '../../../../config/base_response/base_response.dart';
+import '../entity/forget_password_entity.dart';
+
 abstract class AuthRepo {
+
+  Future<BaseResponse<ForgetPasswordEntity>> forgetPassword(String email);
+
+  Future<BaseResponse<ForgetPasswordEntity>> resetCode(String code);
+  Future<BaseResponse<ForgetPasswordEntity>> resetPassword(String email,String newPassword);
+
 }
