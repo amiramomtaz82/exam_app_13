5b19be5ce76230b938b933d4457f8674e122a7c7
beso <bassionymohamed898@gmail.com>
2026-07-31 17:40:31 +0300

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
+import 'package:exam_app_13/config/di.dart';
+import 'package:exam_app_13/core/app_theme/app_theme.dart';
+import 'package:exam_app_13/features/auth/presentaion/sign_Up_in/view/signin_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-import 'config/di.dart';
-import 'core/app_theme/app_theme.dart';
-
 
 void main() {
-
-
   configureDependencies();
   runApp(const MyApp());
 }
@@ -15,18 +11,13 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
-        theme: AppTheme.lightTheme,
-         home: Scaffold()
-      // BlocProvider(
-      //     create: (_) => getIt<ForgetPasswordCubit>(),
-      //     child: const ForgetPasswordScreen(),
-
-      //   )
-       );
+      title: 'Exam App',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      home: const SigninScreen(),
+    );
   }
 }
