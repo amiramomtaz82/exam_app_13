353add97fb679d5cf771a9d8422f4f6e88367eaf
Amira Momtaz <amiraomtaz82@gmail.com>
2026-08-01 18:58:30 +0300

    update validation.dart

--- lib/config/validation/validation.dart
+++ lib/config/validation/validation.dart
@@ -1,4 +1,4 @@
- abstract class Validation {
+abstract class Validation {
   static String? validateEmail(String? value) {
     if (value == null || value.isEmpty) {
       return 'Email is required';
