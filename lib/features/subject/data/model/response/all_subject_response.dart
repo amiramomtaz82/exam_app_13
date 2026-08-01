c4b54f6fe077c83d0eec57a014d23310b6b3c938
Amira Momtaz <amiraomtaz82@gmail.com>
2026-08-02 00:09:03 +0300

    finish subject remote data source and repo implementation

--- lib/features/subject/data/model/response/all_subject_response.dart
+++ lib/features/subject/data/model/response/all_subject_response.dart
@@ -1,3 +1,5 @@
+import 'package:exam_app_13/features/subject/data/model/response/subjectDTO.dart';
+
 /// message : "success"
 /// metadata : {"currentPage":1,"numberOfPages":1,"limit":40}
 /// subjects : [{"_id":"69d980107c82914570305dbd","name":"JavaScript","icon":"https://exam.elevateegy.com/uploads/categories/seeder-javascript.png","createdAt":"2026-04-10T22:56:16.582Z"},{"_id":"69d980107c82914570305dc0","name":"Python","icon":"https://exam.elevateegy.com/uploads/categories/seeder-python.png","createdAt":"2026-04-10T22:56:16.757Z"},{"_id":"69d980107c82914570305dc3","name":"HTML & CSS","icon":"https://exam.elevateegy.com/uploads/categories/seeder-html.png","createdAt":"2026-04-10T22:56:16.940Z"},{"_id":"69d980117c82914570305dc6","name":"Data Struct","icon":"https://exam.elevateegy.com/uploads/categories/seeder-dsa.png","createdAt":"2026-04-10T22:56:17.111Z"},{"_id":"69d980117c82914570305dc9","name":"React.js","icon":"https://exam.elevateegy.com/uploads/categories/seeder-react.png","createdAt":"2026-04-10T22:56:17.279Z"},{"_id":"69d980117c82914570305dcc","name":"Node.js","icon":"https://exam.elevateegy.com/uploads/categories/seeder-nodejs.png","createdAt":"2026-04-10T22:56:17.465Z"},{"_id":"69d980117c82914570305dcf","name":"Databases","icon":"https://exam.elevateegy.com/uploads/categories/seeder-database.png","createdAt":"2026-04-10T22:56:17.634Z"},{"_id":"69d980117c82914570305dd2","name":"Git & DevOp","icon":"https://exam.elevateegy.com/uploads/categories/seeder-git.png","createdAt":"2026-04-10T22:56:17.805Z"},{"_id":"6a568d32d2ea07268fb2d62a","name":"Arabic","icon":"https://exam.elevateegy.com/uploads/categories/6a568d32d2ea07268fb2d628-Microscope.png","createdAt":"2026-07-14T19:25:38.223Z"}]
@@ -14,13 +16,13 @@ class AllSubjectResponse {
     if (json['subjects'] != null) {
       subjects = [];
       json['subjects'].forEach((v) {
-        subjects?.add(Subjects.fromJson(v));
+        subjects?.add(SubjectsDTO.fromJson(v));
       });
     }
   }
   String? message;
   Metadata? metadata;
-  List<Subjects>? subjects;
+  List<SubjectsDTO>? subjects;
 
   Map<String, dynamic> toJson() {
     final map = <String, dynamic>{};
@@ -41,34 +43,7 @@ class AllSubjectResponse {
 /// icon : "https://exam.elevateegy.com/uploads/categories/seeder-javascript.png"
 /// createdAt : "2026-04-10T22:56:16.582Z"
 
-class Subjects {
-  Subjects({
-      this.id, 
-      this.name, 
-      this.icon, 
-      this.createdAt,});
 
-  Subjects.fromJson(dynamic json) {
-    id = json['_id'];
-    name = json['name'];
-    icon = json['icon'];
-    createdAt = json['createdAt'];
-  }
-  String? id;
-  String? name;
-  String? icon;
-  String? createdAt;
-
-  Map<String, dynamic> toJson() {
-    final map = <String, dynamic>{};
-    map['_id'] = id;
-    map['name'] = name;
-    map['icon'] = icon;
-    map['createdAt'] = createdAt;
-    return map;
-  }
-
-}
 
 /// currentPage : 1
 /// numberOfPages : 1
---
+++ lib/features/subject/data/repo/subject_repo_imp.dart
@@ -0,0 +1,42 @@
+
+
+
+import 'package:exam_app_13/config/base_response/base_response.dart';
+import 'package:exam_app_13/features/subject/data/data_source/subject_remote_data_source.dart';
+import 'package:exam_app_13/features/subject/data/model/response/subjectDTO.dart';
+import 'package:exam_app_13/features/subject/domain/entity/subject_entity.dart';
+import 'package:exam_app_13/features/subject/domain/repo/subject_repo.dart';
+import 'package:injectable/injectable.dart';
+
+@Injectable(as:SubjectRepo)
+
+class SubjectRepoImp extends SubjectRepo{
+
+  SubjectRemoteDataSource _subjectRemoteDataSource;
+  SubjectRepoImp (this._subjectRemoteDataSource);
+  @override
+  Future<BaseResponse<List<SubjectEntity>>> getAllSubjects() async{
+
+final BaseResponse<List<SubjectsDTO>> response
+=  await _subjectRemoteDataSource.getAllSubjects();
+
+switch(response){
+
+
+
+  case SuccessResponse<List<SubjectsDTO>>():
+    final List<SubjectEntity> subjectsList=
+    response.data.map((dto)=>dto.toEntity()).toList();
+    
+    return SuccessResponse<List<SubjectEntity>> (subjectsList);
+  case ErrorResponse<List<SubjectsDTO>>():
+return ErrorResponse(errMessage: response.errMessage);
+
+}
+
+
+  }
+
+
+
+}
