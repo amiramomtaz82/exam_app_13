0f12d91fcfe20db255e3f4f91ae106ad99771687
Amira Momtaz <amiraomtaz82@gmail.com>
2026-08-01 23:14:26 +0300

    adding subject api client

--- lib/core/constants/app_strings/endpoints.dart
+++ lib/core/constants/app_strings/endpoints.dart
@@ -3,4 +3,7 @@ class Endpoints {
 
   static const String baseUrl = 'https://exam.elevateegy.com/';
   static const String loginEndPoint = "api/v1/auth/signin";
+
+  static const String getAllSubjectEndPoint="api/v1/subjects";
+
 }
